5974163382a59e1c108e1417941f579877b4fdc3
herve.haudemand <herve.haudemand@inaf.it>
2026-02-03 16:53:24 +0100

    minor refactoring, backup

--- debug.py
+++ debug.py
@@ -15,15 +15,41 @@ import micropolarray as ml
 from micropolarray.processing.demodulation_errors import *
 from micropolarray.processing.image_cleaning import remove_outliers_simple
 from micropolarray.processing.linear_roi import DDA, linear_roi, linear_roi_from_polar
+from astropy.io import fits
+import tracemalloc
+from astropy import units as u
+from pympler import asizeof
+from memory_profiler import profile
 
 
+# @profile
 def main():
-    image = ml.MicropolImage(
-        "/home/herve/dottorato/antarticor/herve/campagna_2022/results/2021_12_11/corona_0/corona.fits"
-    )
+    tracemalloc.start()
 
-    fig, ax, fig2, ax2 = image.show_with_pol_params()
-    plt.show()
+    fname = "/home/herve/dottorato/antarticor/herve/campagna_2022/results/2021_12_11/corona_0/corona.fits"
+    fname = "/home/herve/dottorato/mexicor/eclipse_2024/eclipse_sequence_2024/sample_image_w4096_h3000_t2000_n4_f0_20240408T181639.fits"
+
+    with fits.open(fname) as hdu:
+        ...
+        # data = hdu[0].data
+        # print(type(data[0, 0]))
+
+    demodulator = ml.Demodulator(
+        "/home/herve/dottorato/cormag/2023_flight/post_flight_calibration/polarimetria/demo_matrices_computation/demo_matrices_correctangles/notilt/1"
+    )
+    image = ml.MicropolImage(fname)
+    # image = image.demodulate(demodulator)
+
+    # print((asizeof.asizeof(image) * u.byte).to(u.megabyte))
+    # current, peak = tracemalloc.get_traced_memory()
+    # print(current)
+
+    for key, val in image.__dict__.items():
+        ...
+        # print(key, val, (asizeof.asizeof(val) * u.byte).to(u.megabyte))
+
+    image = None
+    print("Done")
 
 
 if __name__ == "__main__":
